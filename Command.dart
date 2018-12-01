e21ca71c6d73c676024a2d2a9bdc62ff6a4d2be6
tom <a@b>
2018-12-01 19:52:03 +0200

    Cosmetic changes, touch and xargs

--- Command.dart
+++ Command.dart
@@ -6,7 +6,12 @@ abstract class Command {
 
 abstract class BaseCommand extends Command {
     List<String> arguments;
-    BaseCommand(this.arguments);
+    String cmd_name;
+    BaseCommand(this.arguments, this.cmd_name);
+
+    String toString() {
+        return "$cmd_name(" + arguments.join(", ") + ")";
+    }
 }
 
 // Pipe command A into command B
@@ -25,49 +30,41 @@ class CompoundCommand extends Command {
 }
 
 class Echo extends BaseCommand {
-    Echo(List<String> arguments) : super(arguments);
+    Echo(List<String> arguments) : super(arguments, 'echo');
 
     String apply(String stdin, Environment env) {
         return arguments.join(" ");
     }
-
-    String toString() {
-        return "echo(" + arguments.join(', ') + ")";
-    }
 }
 
 class Cat extends BaseCommand {
-    Cat(List<String> arguments) : super(arguments);
+    Cat(List<String> arguments) : super(arguments, 'cat');
 
     String apply(String stdin, Environment env) {
         List<String> datas;
         for(String filename in arguments) {
             if(!env.exists(filename)) {
-                datas.add("Path $filename does not exist");
+                datas.add("cat: can't open '$filename': No such file or directory");
                 continue;
             }
             if(env.get_type(filename) == NodeType.DIRECTORY) {
-                datas.add("$filename is a directory");
+                datas.add("cat: read error: Is a directory");
                 continue;
             }
             datas.add(env.read_file(filename));
         }
         return datas.join("\n");
     }
-
-    String toString() {
-        return "cat(" + arguments.join(', ') + ")";
-    }
 }
 
 class Ls extends BaseCommand {
-    Ls(List<String> arguments) : super(arguments);
+    Ls(List<String> arguments) : super(arguments, 'ls');
 
     String apply(String stdin, Environment env) {
         List<String> datas;
         for(String filename in arguments) {
             if(!env.exists(filename)) {
-                datas.add("Path $filename does not exist");
+                datas.add("ls: $filename: No such file or directory");
                 continue;
             }
             if(env.get_type(filename) == NodeType.FILE) {
@@ -79,14 +76,42 @@ class Ls extends BaseCommand {
         }
         return datas.join("\n");
     }
+}
 
-    String toString() {
-        return "ls(" + arguments.join(', ') + ")";
+class Touch extends BaseCommand {
+    Touch(List<String> arguments) : super(arguments, 'touch');
+
+    String apply(String stdin, Environment env) {
+        List<String> datas;
+        for(String path in arguments) {
+            if(env.exists(path)) {
+                continue;
+            }
+            try {
+                env.create_new(path);
+            } on FileException catch(e) {
+                datas.add("touch: $path: No such file or directory");
+            }
+        }
+        return datas.join("\n");
+    }
+}
+
+class Xargs extends BaseCommand {
+    Xargs(List<String> arguments) : super(arguments, 'xargs');
+
+    String apply(String stdin, Environment env) {
+        if(arguments.length == 0) {
+            return "xargs: no command specified";
+        }
+        BaseCommand cmd_to_run = parse_atomic_command(
+            arguments.join(" ") + " " + stdin);
+        return cmd_to_run.apply("", env);
     }
 }
 
 class EmptyCommand extends BaseCommand {
-    EmptyCommand(List<String> arguments) : super(arguments) {
+    EmptyCommand(List<String> arguments) : super(arguments, 'empty_command') {
         assert(arguments.length == 0);
     }
 
