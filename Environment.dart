55b855d0220d791c9f8edcb423eb51dad003fc27
az <az@a.com>
2018-12-01 18:35:25 +0200

    Implement basic filesystem Implement exists()

--- Environment.dart
+++ Environment.dart
@@ -1,20 +1,81 @@
-import 'FileSystem.dart';
+class FileException implements Exception {}
 
 class Environment {
     FileSystem filesys;
-    Map<String, String> env_variables;
 
-    Environment(this.filesys) {
-        env_variables["pwd"] = "/";
+    Node curDir;
+
+    Environment() {
+        filesys = FileSystem();
+        curDir = filesys.root;
     }
 
-    String sanitize_path(String unsanitized_path, FileSystem filesys) {
-        String absolute_path;
-        if(is_absolute_directory(unsanitized_path)) {
-            absolute_path = unsanitized_path;
+    String absolute_path(String path) {
+        var end_node = node_at(path);
+        if (end_node == null)
+            throw FileException();
+        return path_to_node(end_node);
+    }
+
+    String path_to_node(Node node) {
+        String result = '';
+        if (identical(node, filesys.root))
+            return '/';
+        while (!identical(node, filesys.root)) {
+            result = '/${node.name}' + result;
+            node = node.parent;
         }
-        else {
-            absolute_path = env_variables["pwd"] + unsanitized_path;
+        return result;
+    }
+
+    Node node_at(String path) {
+        Node ptr;
+        if (path == '')
+            return curDir;
+        if (path[0] == '/') {
+            ptr = filesys.root;
+            path = path.substring(1);
+        } else {
+            ptr = curDir;
         }
+        for (var component in path.split("/")) {
+            if (ptr.type != NodeType.DIRECTORY)
+                return null;
+            if (component == '.')
+                continue;
+            if (component == '..') {
+                ptr = ptr.parent;
+                continue;
+            }
+            ptr = ptr.get_child(component);
+        }
+        return ptr;
+    }
+
+    bool exists(String path) => node_at(path) != null;
+}
+
+enum NodeType {
+    FILE, DIRECTORY, LINK
+}
+
+class Node {
+    // A directory or file
+    Map<String, Node> children;
+    NodeType type;
+    Node parent;
+    String name;
+
+    Node get_child(String component) => children[component];
+}
+
+class FileSystem {
+    Node root;
+
+    FileSystem() {
+        root = Node();
+        root.parent = root;
+        root.type = NodeType.DIRECTORY;
+        root.children = Map<String, Node>();
     }
 }
