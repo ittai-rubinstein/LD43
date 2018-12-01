9a6dafbf7f1749b9fa23763be417a77ce09fe270
arealuser <noone@nobody>
2018-12-01 19:45:30 +0200

    removed the <<<HEAD

--- main.dart
+++ main.dart
@@ -1,19 +1,6 @@
-<<<<<<< HEAD
+
 import 'Console.dart';
-import "Command.dart";
-import 'dart:io';
 
 void main() {
     var con = new Console();
-    while(true) {
-        stdout.writeln('Enter a command:');
-        String input = stdin.readLineSync();
-        try {
-            Command cmd = parse_command(input);
-            stdout.writeln(cmd);
-        }
-        on ParseException catch (e) {
-            stdout.writeln("Error: " + e.cause);
-        }
-    }
 }
